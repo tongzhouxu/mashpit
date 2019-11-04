bbd3b0acf1f96aec66cc4d6f3df7322dcfa924f1
Lee Katz - Aspen <gzu2@cdc.gov>
2019-11-04 14:39:23 -0500

    added more fields from looking at PHAC biosamples

--- scripts/mashpit.sql
+++ scripts/mashpit.sql
@@ -2,20 +2,28 @@ CREATE TABLE IF NOT EXISTS TAXONOMY(
   taxid     INTEGER PRIMARY KEY, /* same as NCBI taxid wherever possible */
   genus     TEXT,
   species   TEXT,
-  UNIQUE    (genus, species)
+  subspecies TEXT,
+  UNIQUE    (genus, species, subspecies)
 );
 
 CREATE TABLE IF NOT EXISTS BIOSAMPLE(
   biosample_acc  INTEGER PRIMARY KEY AUTOINCREMENT,
   ncbiacc   TEXT, /* Usually starts with SAMN, SAME, or something similar */
   strain    TEXT, /* Allow it to be null */
+  isolate   TEXT,
   taxid     INTEGER,
+  collected_by TEXT,
   collection_date TEXT,
   latitude  FLOAT,
   longitude FLOAT,
+  host_taxid INTEGER,
+  host_disease TEXT,
+  isolation_source TEXT,
+  serovar   TEXT,
   UNIQUE    (ncbiacc),
   UNIQUE    (strain),
   FOREIGN KEY (taxid) REFERENCES TAXONOMY(taxid)
+  FOREIGN KEY (host_taxid) REFERENCES TAXONOMY(taxid)
 );
 
 CREATE TABLE IF NOT EXISTS SKETCH(
