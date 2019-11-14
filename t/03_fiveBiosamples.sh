868be4b2376d443d57227f2a08874959d7d1faae
Lee Katz <lskatz@gmail.com>
2019-11-13 22:12:19 -0500

    removed a possibly bad biosample from testing

--- t/03_fiveBiosamples.sh
+++ t/03_fiveBiosamples.sh
@@ -11,9 +11,9 @@ script=$here/../scripts/addFromNcbi.sh
 @test "Add SAMN02182866 concurrently" {
   $script $db SAMN02182866
 }
-@test "Add SAMN02182867 concurrently" {
-  $script $db SAMN02182867
-}
+#@test "Add SAMN02182867 concurrently" {
+#  $script $db SAMN02182867
+#}
 @test "Add SAMN02182868 concurrently" {
   $script $db SAMN02182868
 }
