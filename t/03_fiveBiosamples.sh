d58f01c364e6292995b7590d89e36af83c534396
Lee Katz <lskatz@gmail.com>
2019-11-13 21:31:11 -0500

    concurrently label

--- t/03_fiveBiosamples.sh
+++ t/03_fiveBiosamples.sh
@@ -5,19 +5,19 @@ db=$here/test.sqlite
 script=$here/../scripts/addFromNcbi.sh
 
 # Download five at once, test database locking, etc
-@test "Add SAMN02182865" {
+@test "Add SAMN02182865 concurrently" {
   $script $db SAMN02182865
 }
-@test "Add SAMN02182866" {
+@test "Add SAMN02182866 concurrently" {
   $script $db SAMN02182866
 }
-@test "Add SAMN02182867" {
+@test "Add SAMN02182867 concurrently" {
   $script $db SAMN02182867
 }
-@test "Add SAMN02182868" {
+@test "Add SAMN02182868 concurrently" {
   $script $db SAMN02182868
 }
-@test "Add SAMN02182869" {
+@test "Add SAMN02182869 concurrently" {
   $script $db SAMN02182869
 }
 
