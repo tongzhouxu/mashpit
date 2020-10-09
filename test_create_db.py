3f382e27bd776e46aad9e331b3ba674a12d91042
Tongzhou Xu <tongzhouxu@MacBook-Pro.attlocal.net>
2020-10-08 23:41:29 -0400

    first unit test

---
+++ test_create_db.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from unittest.mock import MagicMock, Mock
+from create_db import create_connection
+import unittest
+import sqlite3
+
+
+class MyTests(unittest.TestCase):
+
+    def test_sqlite3_connect_success(self):
+        sqlite3.connect = MagicMock(return_value='connection succeeded')
+        cc = create_connection('test.db')
+        sqlite3.connect.assert_called_with('test.db', check_same_thread=False)
+        self.assertEqual(cc, 'connection succeeded')
+
+    def test_sqlite3_connect_fail(self):
+        sqlite3.connect = MagicMock(return_value='connection failed')
+        cc = create_connection('test.db')
+        sqlite3.connect.assert_called_with('test.db', check_same_thread=False)
+        self.assertEqual(cc, 'connection failed')
+
+
+if __name__ == '__main__':
+    unittest.main()
