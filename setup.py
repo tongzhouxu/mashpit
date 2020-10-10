8ccdd35180c60cd682bf91c1ba5590260b8cbff8
Tongzhou Xu <tongzhouxu@MacBook-Pro.attlocal.net>
2020-10-09 21:34:58 -0400

    add setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mashpit',
     description='A sketch-based surveillance platform',
     package_dir={'': 'scripts'},
+    packages=find_packages(where='scripts'),
 )
