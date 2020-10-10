8b90223a07d1d979c44909fa5c87dfb9049be2e3
Tongzhou Xu <tongzhouxu@MacBook-Pro.attlocal.net>
2020-10-09 21:22:48 -0400

    add setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from setuptools import setup
 setup(
     name='mashpit',
     description='A sketch-based surveillance platform',
-    py_modules=['create_db','metadata_sra_db','query_against_db','sketch_db'],
+    package_dir={'': 'scripts'},
 )
