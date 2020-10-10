e1c8708831b7cc6c056f57d07c77d52925c23200
Tongzhou Xu <tongzhouxu@MacBook-Pro.attlocal.net>
2020-10-09 21:04:00 -0400

    add setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mashpit',
     description='A sketch-based surveillance platform',
-    scripts=[
-        'scripts/create_db.py',
-        'scripts/metadata_sra_db.py',
-        'scripts/query_against_db.py',
-        'scripts/sketch_db.py',
-    ],
+    packages=find_packages(where='scripts'),
 )
