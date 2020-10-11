ce5f6dd3bc9ed5172d11251fefd58a39b1886887
Tongzhou Xu <tongzhouxu@MacBook-Pro.attlocal.net>
2020-10-11 00:39:48 -0400

    update test_create_db

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='mashpit',
     description='A sketch-based surveillance platform',
     packages=['scripts'],
+    scripts=[
+        'scripts/create_db.py',
+        'scripts/metadata_sra_db.py',
+        'scripts/query_against_db.py',
+        'scripts/sketch_db.py',
+    ],
 )
--- tests/test_create_db.py
+++ tests/test_create_db.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock
 from scripts.create_db import create_connection
+import subprocess
 import unittest
 import sqlite3
 
@@ -19,6 +20,23 @@ class MyTests(unittest.TestCase):
         cc = create_connection('test.db')
         sqlite3.connect.assert_called_with('test.db', check_same_thread=False)
         self.assertEqual(cc, 'connection failed')
+
+    def test_script(self):
+        result_help = subprocess.run(['create_db.py', '-h'], capture_output=True)
+        subprocess.run("create_db.py test", shell=True)
+        conn = create_connection('test.db')
+        c = conn.cursor()
+        c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='BIOSAMPLE' ''')
+        self.assertIsNot(c.fetchone()[0], 0)
+        self.assertIn(b'usage: create_db.py <database name>\n\npositional arguments:\n  database    <string>: name of '
+                      b'the database\n\noptional arguments:\n  -h, --help  show this help message and exit\n',
+                      result_help.stdout)
+
+    def test_script_failure(self):
+        result = subprocess.run(['create_db.py', '--database', 'test'], capture_output=True)
+        result_no_args = subprocess.run(['create_db.py'], capture_output=True)
+        self.assertEqual(result.returncode, 2)
+        self.assertEqual(result_no_args.returncode, 2)
 
 
 if __name__ == '__main__':
