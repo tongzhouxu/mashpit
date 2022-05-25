15f440fdc4432ffce701fab301735633d921f2a5
Tongzhou Xu <tongzhou@s172-29-1-h195.griffin.uga.edu>
2022-05-25 09:19:02 -0400

    0.9.2

--- mashpit/__init__.py
+++ mashpit/__init__.py
@@ -1,3 +1,3 @@
 __author__ = 'Tongzhou Xu'
 __email__ = 'tongzhou.xu@uga.edu'
-__version__ = '0.9.1'
+__version__ = '0.9.2'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as rm:
 
 setup(
     name='mashpit',
-    version='0.9.1',
+    version='0.9.2',
     url='https://github.com/tongzhouxu/mashpit',
     author='Tongzhou Xu',
     author_email='tongzhou.xu@uga.edu',
